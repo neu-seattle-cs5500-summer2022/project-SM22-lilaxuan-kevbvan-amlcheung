61525a326f4aaaf2c174fe34cf38c50618837084
Ashley Cheung <ashleycheung@Ashleys-MacBook-Air.local>
2022-06-29 22:13:50 -0700

    adding front end starter code

--- events_project/src/App.js
+++ events_project/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  const [restaurants, setRestaurants] = useState([]);
+  //const [newHomeInput, setNewHomeInput] = useState('');
+
+  function getRestaurants() {
+    Axios.get('/api/restaurant/')
+      .then(function(response) {
+        setRestaurants(response.data);
+      })
+  }
+  useEffect(getRestaurants, []);
+  console.log(restaurants);
+
+  const restaurantComponent = [];
+  console.log(restaurantComponent);
+  for(let restaurant of restaurants) {
+    console.log(restaurant);
+    restaurantComponent.push(<div>
+      <a href={'/restaurant/' + restaurant._id}><div className="restaurant-name font-style-restaurants">{restaurant.name}</div></a>
+      <div className="attribute-name font-style-restaurants">Cuisine: {restaurant.cuisine}</div>
+      <div className="attribute-name font-style-restaurants margin">Michelin Stars: {restaurant.rating}</div>
+      </div>)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+        <div className="font-style-restaurants">
+          {restaurantComponent}
+        </div>
+    </body>
   );
 }
 
-export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import React, {}  from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
--- events_project/src/index.js
+++ events_project/src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NavBar from './NavBar';
+import Login from './Login';
+import CreateUser from './CreateUser';
+import RestaurantEntry from './RestaurantEntry';
+import RestaurantPage from './RestaurantPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  ReactDOM.render(
+   <div>
+   <BrowserRouter>
+    <NavBar />
+     <Routes>
+      <Route path={"/"} element={<App />}/>
+      <Route path={"/login"} element={<Login />} />
+      <Route path={"/createUser"} element={<CreateUser />} />
+      <Route path={"/restaurantEntry/:restaurantId"} element={<RestaurantEntry />}/>
+      <Route path={"/restaurant/:restaurantId"} element={<RestaurantPage />}/>
+    </Routes>
+   </BrowserRouter>
+      
+   </div>
+ ,
+   document.getElementById('root') );
